c799c3939c024e6065f41d69e397381a999931f7
Mohib2021 <mdmohibullasheikh@gmail.com>
2021-10-22 21:41:55 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const nameRef = useRef();
+	const emailRef = useRef();
+	const [users, setUsers] = useState([]);
+	useEffect(() => {
+		fetch("http://localhost:5000/users")
+			.then((res) => res.json())
+			.then((data) => setUsers(data));
+	}, []);
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		const name = nameRef.current.value;
+		const email = emailRef.current.value;
+		const NewUser = { name, email };
+		fetch("http://localhost:5000/users", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify(NewUser),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				const updatedData = [...users, data];
+				setUsers(updatedData);
+			});
+		nameRef.current.value = "";
+		emailRef.current.value = "";
+	};
+	return (
+		<div className="App">
+			<h2>Total Users : {users.length}</h2>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>
+						Id : {user.id} Name : {user.name} Email : {user.email}{" "}
+					</li>
+				))}
+			</ul>
+			<form onSubmit={handleFormSubmit}>
+				<input ref={nameRef} type="text" name="" id="" placeholder="Name" />
+				<input ref={emailRef} type="email" name="" id="" placeholder="Email" />
+				<input type="submit" value="submit" />
+			</form>
+		</div>
+	);
 }
 
 export default App;
